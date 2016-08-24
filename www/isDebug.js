dfbe84d71f7ce4b696fee21b6f1a58ae41862259
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-08-24 15:31:48 +0100

    Revert "Add debugging"
    This reverts commit d017d6e9751a253fe1ed629142128f2577d17cd6.

--- www/isDebug.js
+++ www/isDebug.js
@@ -2,7 +2,6 @@ var exec = require('cordova/exec');
 
 exports.getIsDebug = function(success, error) {
   exec(function(isDebug) {
-    console.log('ORIGINAL VALUE', isDebug);
     success(!!isDebug); // workaround for android not being able to return actual 1 or 0
   }, error, "IsDebug", "getIsDebug", []);
 };
