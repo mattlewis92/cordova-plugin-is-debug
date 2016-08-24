d017d6e9751a253fe1ed629142128f2577d17cd6
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-08-24 15:29:53 +0100

    Add debugging

--- www/isDebug.js
+++ www/isDebug.js
@@ -2,6 +2,7 @@ var exec = require('cordova/exec');
 
 exports.getIsDebug = function(success, error) {
   exec(function(isDebug) {
+    console.log('ORIGINAL VALUE', isDebug);
     success(!!isDebug); // workaround for android not being able to return actual 1 or 0
   }, error, "IsDebug", "getIsDebug", []);
 };
