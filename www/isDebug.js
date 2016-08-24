c4c62750373f01eec1025dba20772fca895e366a
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-08-24 15:34:42 +0100

    Fix comment

--- www/isDebug.js
+++ www/isDebug.js
@@ -2,6 +2,6 @@ var exec = require('cordova/exec');
 
 exports.getIsDebug = function(success, error) {
   exec(function(isDebug) {
-    success(!!isDebug); // workaround for android not being able to return actual 1 or 0
+    success(!!isDebug); // workaround for android not being able to booleans, and only 1 or 0 instead
   }, error, "IsDebug", "getIsDebug", []);
 };
